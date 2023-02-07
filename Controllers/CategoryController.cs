d7f563a8207006ef243aa2c5d238f0ce9d002205
VAN1LLA <tlink1776@gmail.com>
2023-02-07 11:23:10 -0500

    Continued Development

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Controllers.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -14,8 +15,13 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var objCategoryList = _db.Categories.ToList(); 
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList(); 
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
